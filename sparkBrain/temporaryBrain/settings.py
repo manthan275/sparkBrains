abd4e12ca0ee0c0480418458dd029e21285f265c
Shivangi <sharma.shivangi0077@gmail.com>
2020-11-22 19:11:30 +0530

    Updated commit

--- sparkBrain/temporaryBrain/settings.py
+++ sparkBrain/temporaryBrain/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'temporaryBrain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'demo_db',
-        'PASSWORD': 'password',
-        'USER': 'support',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
